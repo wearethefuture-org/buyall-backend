5fd8fb8c33583e464ac731ffd10bc9c6d65f3606
Pavel <pavel.makk.old@gmail.com>
2019-11-04 21:54:24 +0200

    Services refactoring

--- src/routes/subCategories/handlers/subCategoriesHandlers.js
+++ src/routes/subCategories/handlers/subCategoriesHandlers.js
@@ -1,29 +1,38 @@
-const SubCategoryService = new (require('../../../services/subCategory'))();
+const SubCategoryService = require('../../../services/subCategory');
 
 const getSubCategories = async ctx => {
-    ctx.response.body = await SubCategoryService.getSubCategories();
+    const subCategoryService = new SubCategoryService();
+
+    ctx.response.body = await subCategoryService.getSubCategories();
 };
 
 const getSubCategory = async ctx => {
+    const subCategoryService = new SubCategoryService();
     const { id } = ctx.params;
-    ctx.response.body = await SubCategoryService.getSubCategory(id);
+
+    ctx.response.body = await subCategoryService.getSubCategory(id);
 };
 
 const createtSubCategory = async ctx => {
+    const subCategoryService = new SubCategoryService();
     const newSubCategory = ctx.request.body;
-    ctx.response.body = await SubCategoryService.createSubCategory(newSubCategory);
+
+    ctx.response.body = await subCategoryService.createSubCategory(newSubCategory);
 };
 
 const updateSubCategory = async ctx => {
+    const subCategoryService = new SubCategoryService();
     const { id } = ctx.params;
     const updatedSubCategory = ctx.request.body;
 
-    ctx.response.body = await SubCategoryService.updateSubCategory(id, updatedSubCategory);
+    ctx.response.body = await subCategoryService.updateSubCategory(id, updatedSubCategory);
 };
 
 const deleteSubCategory = async ctx => {
+    const subCategoryService = new SubCategoryService();
     const {id} = ctx.params;
-    ctx.response.body = await SubCategoryService.deleteSubCategory(id);
+
+    ctx.response.body = await subCategoryService.deleteSubCategory(id);
 };
 
 module.exports = {
