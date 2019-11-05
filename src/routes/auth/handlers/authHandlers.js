75b56082f1c4a008b1bd2e10297727c600adcb8f
Pavel <pavel.makk.old@gmail.com>
2019-11-05 16:54:35 +0200

    Mail service

--- src/routes/auth/handlers/authHandlers.js
+++ src/routes/auth/handlers/authHandlers.js
@@ -1,4 +1,15 @@
+const MailService = require('../../../services/mail');
+
 const login = async ctx => {
+    const mailService = new MailService();
+    var email = {
+        from: 'buyall@gmail.com',
+        to: '7734.why.not@gmail.com',
+        subject: 'Test 2',
+        text: 'Text2 ',
+        html: '<b>Test 2</b>'
+    };
+    mailService.sendMail(email)
     ctx.response.body = 'login';
 };
 
