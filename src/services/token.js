c6976a548ef676e591e1cde66a7696657352026c
Pavel <pavel.makk.old@gmail.com>
2019-11-13 11:41:32 +0200

    Token service, generate token method

---
+++ src/services/token.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken');
+
+
+class TokenService extends BaseModel {
+    async generateToken(payload, expiresIn) {
+        // replace it to base service
+        const secretKey = process.env.SECRET_KEY;
+        return jwt.sign(payload, secretKey, {expiresIn});
+    }
+}
+
+module.exports = TokenService;
