20157355802194dde31fb35138a12c28cf017a36
Pavel <pavel.makk.old@gmail.com>
2019-11-28 12:44:28 +0200

    Products table relationships with sub categories

--- src/db/models/relations.js
+++ src/db/models/relations.js
@@ -1,43 +1,57 @@
 module.exports = db => {
-  db.subCategories.belongsTo(db.categories, {
-   as: db.aliases.subCategories.categories,
-    field: 'category_id',
-    targetKey: 'id',
-    foreignKey: 'categoryId',
-    onDelete: 'CASCADE'
-  });
   db.categories.hasMany(db.subCategories, {
-   as: db.aliases.categories.subCategories,
+    as: db.aliases.categories.subCategories,
     field: 'category_id',
     targetKey: 'id',
     foreignKey: 'categoryId',
   });
-
-  db.usersKeys.belongsTo(db.users, {
-   as: db.aliases.usersKeys.users,
-    field: 'user_id',
+  db.subCategories.belongsTo(db.categories, {
+    as: db.aliases.subCategories.categories,
+    field: 'category_id',
     targetKey: 'id',
-    foreignKey: 'userId',
+    foreignKey: 'categoryId',
     onDelete: 'CASCADE'
   });
+
+  db.subCategories.hasMany(db.products, {
+    as: db.aliases.subCategories.products,
+    field: 'sub_category_id',
+    targetKey: 'id',
+    foreignKey: 'subCategoryId',
+  });
+  db.products.belongsTo(db.subCategories, {
+    as: db.aliases.products.subCategories,
+    field: 'sub_category_id',
+    targetKey: 'id',
+    foreignKey: 'subCategoryId',
+    onDelete: 'CASCADE'
+  });
+
   db.users.hasMany(db.usersKeys, {
-   as: db.aliases.users.usersKeys,
+    as: db.aliases.users.usersKeys,
     field: 'user_id',
     targetKey: 'id',
     foreignKey: 'userId',
   });
-
-  db.usersForgotPasswords.belongsTo(db.users, {
-   as: db.aliases.usersForgotPasswords.users,
+  db.usersKeys.belongsTo(db.users, {
+    as: db.aliases.usersKeys.users,
     field: 'user_id',
     targetKey: 'id',
     foreignKey: 'userId',
     onDelete: 'CASCADE'
   });
+
   db.users.hasMany(db.usersForgotPasswords, {
-   as: db.aliases.users.usersForgotPasswords,
+    as: db.aliases.users.usersForgotPasswords,
     field: 'user_id',
     targetKey: 'id',
     foreignKey: 'userId',
+  });
+  db.usersForgotPasswords.belongsTo(db.users, {
+    as: db.aliases.usersForgotPasswords.users,
+    field: 'user_id',
+    targetKey: 'id',
+    foreignKey: 'userId',
+    onDelete: 'CASCADE'
   });
 };
